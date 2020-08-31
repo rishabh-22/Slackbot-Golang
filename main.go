1f7ec811b8399ba67e76247bf73b9b9fee382bbd
Rishabh Bhardwaj <rishabh.bh22@gmail.com>
2020-09-01 00:42:28 +0530

    salckbot in golang which replies to user queries.

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/Krognol/go-wolfram"
+	"github.com/christianrondeau/go-wit"
+	"github.com/nlopes/slack"
+)
+
+const confidenceThreshold = 0.5
+
+var (
+	slackClient   *slack.Client
+	witClient     *wit.Client
+	wolframClient *wolfram.Client
+)
+
+func main() {
+	slackClient = slack.New(os.Getenv("SLACK_ACCESS_TOKEN"))
+	witClient = wit.NewClient(os.Getenv("WIT_AI_ACCESS_TOKEN"))
+	wolframClient = &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
+
+	rtm := slackClient.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			if len(ev.BotID) == 0 {
+				go handleMessage(rtm, ev)
+			}
+		}
+	}
+}
+
+func handleMessage(rtm *slack.RTM, ev *slack.MessageEvent) {
+	result, err := witClient.Message(ev.Msg.Text)
+	if err != nil {
+		log.Printf("unable to get wit.ai result: %v", err)
+		return
+	}
+
+	var (
+		topEntity    wit.MessageEntity
+		topEntityKey string
+	)
+
+	for key, entityList := range result.Entities {
+		for _, entity := range entityList {
+			if entity.Confidence > confidenceThreshold && entity.Confidence > topEntity.Confidence {
+				topEntity = entity
+				topEntityKey = key
+			}
+		}
+	}
+
+	replyToUser(rtm, ev, topEntity, topEntityKey)
+}
+
+func replyToUser(rtm *slack.RTM, ev *slack.MessageEvent, topEntity wit.MessageEntity, topEntityKey string) {
+	switch topEntity.Value {
+	case "greeting":
+		rtm.SendMessage(rtm.NewOutgoingMessage("Yo what up!?", ev.Channel))
+		return
+	case "wolfram_search_query":
+		res, err := wolframClient.GetSpokentAnswerQuery(ev.Text, wolfram.Metric, 1000)
+		if err == nil {
+			rtm.SendMessage(rtm.NewOutgoingMessage(res, ev.Channel))
+			return
+		}
+
+		log.Printf("unable to get data from wolfram: %v", err)
+	}
+	rtm.SendMessage(rtm.NewOutgoingMessage("¯\\_(o_o)_/¯", ev.Channel))
+}
